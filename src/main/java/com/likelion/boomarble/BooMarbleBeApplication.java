5542b80038f9ce0b221e03f11ad0b4152d7c767a
yoojung.choi <green980611@naver.com>
2023-10-11 19:41:23 +0900

    inital commit

---
+++ src/main/java/com/likelion/boomarble/BooMarbleBeApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.boomarble;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooMarbleBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BooMarbleBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/likelion/boomarble/BooMarbleBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelion.boomarble;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooMarbleBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
