de617055d25dde07a02e630865ae82edb25f1045
Zena0128 <0128ja@gmail.com>
2023-10-22 23:18:48 +0900

    Comment: Country enum 주석 삭제

--- src/main/java/com/likelion/boomarble/domain/model/Country.java
+++ src/main/java/com/likelion/boomarble/domain/model/Country.java
@@ -3,7 +3,6 @@ package com.likelion.boomarble.domain.model;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-// 아직 24-1 7+1 비영어권 모집이 발표가 안 나서 추가 예정 (10/22)
 @Getter
 @AllArgsConstructor
 public enum Country {
