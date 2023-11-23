d79a765c02ee102850842cbfc15fc898d58293fd
yoojung.choi <green980611@naver.com>
2023-11-23 21:35:21 +0900

    Feat: 대학 정보 검색 및 상세 조회

--- src/main/java/com/likelion/boomarble/domain/universityInfo/repository/UniversityInfoRepository.java
+++ src/main/java/com/likelion/boomarble/domain/universityInfo/repository/UniversityInfoRepository.java
@@ -5,6 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UniversityInfoRepository extends JpaRepository<UniversityInfo, Long>, JpaSpecificationExecutor<UniversityInfo> {
+    List<UniversityInfo> findAllByNameContaining(String keyword);
 }
--- src/main/java/com/likelion/boomarble/domain/universityInfo/service/UniversityInfoServiceImpl.java
+++ src/main/java/com/likelion/boomarble/domain/universityInfo/service/UniversityInfoServiceImpl.java
@@ -3,7 +3,9 @@ package com.likelion.boomarble.domain.universityInfo.service;
 import com.likelion.boomarble.domain.model.Country;
 import com.likelion.boomarble.domain.model.ExType;
 import com.likelion.boomarble.domain.universityInfo.domain.UniversityInfo;
+import com.likelion.boomarble.domain.universityInfo.dto.UniversityInfoDetailDTO;
 import com.likelion.boomarble.domain.universityInfo.dto.UniversityInfoListDTO;
+import com.likelion.boomarble.domain.universityInfo.exception.UniversityInfoNotFoundException;
 import com.likelion.boomarble.domain.universityInfo.repository.UniversityInfoRepository;
 import com.likelion.boomarble.domain.universityInfo.specification.UniversityInfoSpecifications;
 import lombok.RequiredArgsConstructor;
@@ -32,5 +34,19 @@ public class UniversityInfoServiceImpl implements UniversityInfoService{
 
         List<UniversityInfo> results = universityInfoRepository.findAll(spec);
         return UniversityInfoListDTO.from(results);
+    }
+    @Override
+    @Transactional
+    public UniversityInfoDetailDTO getUniversityInfoDetail(long universityId) {
+        UniversityInfo universityInfo = universityInfoRepository.findById(universityId)
+                .orElseThrow(() -> new UniversityInfoNotFoundException("해당 대학 정보가 없습니다."));
+        return UniversityInfoDetailDTO.from(universityInfo);
+    }
+
+    @Override
+    @Transactional
+    public UniversityInfoListDTO searchUniversityInfoList(String keyword) {
+        List<UniversityInfo> universityInfoList = universityInfoRepository.findAllByNameContaining(keyword);
+        return UniversityInfoListDTO.from(universityInfoList);
     }
 }
