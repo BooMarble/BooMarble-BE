6e6d8776651ed7ed637e39825cdc469f4f613fae
yoojung.choi <green980611@naver.com>
2023-12-03 15:50:21 +0900

    Chore: Cors 설정 수정

--- src/main/java/com/likelion/boomarble/global/config/SecurityConfig.java
+++ src/main/java/com/likelion/boomarble/global/config/SecurityConfig.java
@@ -22,9 +22,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .cors().and()
                 .formLogin().disable()
                 .httpBasic().disable()
-                .cors().disable()
                 .csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
