c1aff21cf545260e15b044da3d2096d9f788cf3a
yoojung.choi <green980611@naver.com>
2023-12-12 22:21:01 +0900

    Docs: 인증서/개인키 경로 수정 및 쉡 스크립트 수정

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -9,7 +9,7 @@ domains=boomarble.com
 rsa_key_size=4096
 data_path="./data/certbot" # certbot 폴더 경로
 email="green980611@hufs.ac.kr"
-staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
