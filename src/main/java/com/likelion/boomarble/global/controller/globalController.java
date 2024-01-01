ce49ae12307cb76c84ec72b5ac4ddc1e5d2038e0
yoojung.choi <green980611@naver.com>
2024-01-01 21:19:24 +0900

    Chore: 필요없는 Import문 제거

--- src/main/java/com/likelion/boomarble/global/controller/globalController.java
+++ src/main/java/com/likelion/boomarble/global/controller/globalController.java
@@ -6,13 +6,11 @@ import com.likelion.boomarble.domain.universityInfo.service.UniversityInfoServic
 import com.likelion.boomarble.global.service.RedisService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.Set;
 
 
 @RestController
