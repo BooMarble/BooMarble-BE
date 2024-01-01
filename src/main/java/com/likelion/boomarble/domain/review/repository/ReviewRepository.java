c87a924bdfdee1bf9a93b0be4a635b9be4ae28be
Zena0128 <0128ja@gmail.com>
2024-01-01 21:18:18 +0900

    feat: 리뷰 최신순/스크랩순 정렬 구현

--- src/main/java/com/likelion/boomarble/domain/review/repository/ReviewRepository.java
+++ src/main/java/com/likelion/boomarble/domain/review/repository/ReviewRepository.java
@@ -10,8 +10,21 @@ import java.util.List;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
-    @Query("SELECT r FROM Review r WHERE r.universityInfo.id = :universityInfoId")
+    @Query("SELECT r FROM Review r WHERE r.universityInfo.id = :universityInfoId ORDER BY r.id DESC")
     List<Review> findByUniversityInfo_Id(@Param("universityInfoId") long universityInfoId);
+<<<<<<< HEAD
 
 
 }
+=======
+    @Query("SELECT r, COUNT(s) AS scrapCount " +
+            "FROM Review r " +
+            "LEFT JOIN Scrap s " +
+            "ON r.id = s.review.id " +
+            "WHERE r.universityInfo.id = :universityInfoId " +
+            "GROUP BY r " +
+            "ORDER BY scrapCount DESC")
+    List<Review> findByUniversityInfo_Id_OrderByScrapCount(@Param("universityInfoId") long universityInfoId);
+    List<Review> findAllByWriter(User user);
+}
+>>>>>>> f891b66 (feat: 리뷰 최신순/스크랩순 정렬 구현)
