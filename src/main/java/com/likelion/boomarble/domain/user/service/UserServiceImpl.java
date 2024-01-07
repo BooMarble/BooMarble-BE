17602f10c0186cbeada2689c69d76fba82a45ca9
Gaeun Lee <rk243812@gmail.com>
2024-01-07 13:04:23 +0900

    Feat: 소셜 로그인 구현

--- src/main/java/com/likelion/boomarble/domain/user/service/UserServiceImpl.java
+++ src/main/java/com/likelion/boomarble/domain/user/service/UserServiceImpl.java
@@ -5,7 +5,6 @@ import com.likelion.boomarble.domain.user.dto.UserSignUpRequestDto;
 import com.likelion.boomarble.domain.user.repository.UserRepository;
 import com.likelion.boomarble.domain.user.token.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -26,10 +25,6 @@ public class UserServiceImpl implements UserService {
         if (userRepository.findByEmail(requestDto.getEmail()).isPresent()){
             throw new Exception("이미 존재하는 이메일입니다.");
         }
-
-//        if (!requestDto.getPassword().equals(requestDto.getCheckedPassword())){
-//            throw new Exception("비밀번호가 일치하지 않습니다.");
-//        }
 
         User user = userRepository.save(requestDto.toEntity());
 
