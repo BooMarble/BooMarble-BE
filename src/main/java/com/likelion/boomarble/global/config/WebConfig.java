9ad2dbf8c8cc431e826c5bec31d03dce72e98963
Zena0128 <0128ja@gmail.com>
2024-01-18 01:53:59 +0900

    Chore: 배포된 url CORS매핑에 추가

--- src/main/java/com/likelion/boomarble/global/config/WebConfig.java
+++ src/main/java/com/likelion/boomarble/global/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "https://boomarble.com", "https://www.boomarble.com")
+                .allowedOrigins("http://localhost:3000", "https://boomarble.com", "https://www.boomarble.com", "https://boo-marble-fe.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true)
                 .allowedHeaders("X-AUTH-TOKEN", "content-type")
